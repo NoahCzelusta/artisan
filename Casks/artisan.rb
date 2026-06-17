98880210da4c030b914bc8b2ea7e06bee0a084c6
Noah Czelusta <156704066+NoahCzelusta@users.noreply.github.com>
2026-06-16 20:41:27 -0700

    Update Homebrew cask for v0.0.4 (#29)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Casks/artisan.rb
+++ Casks/artisan.rb
@@ -1,6 +1,6 @@
 cask "artisan" do
-  version "0.0.3"
-  sha256 "7a656957fd61dc2384b927ae7f568bc2f1ea3a46de184b5255e7d8948e341bbb"
+  version "0.0.4"
+  sha256 "1f615dd2ad56495709344bbcac639979a7d201057a83a5495978c93e308e96bc"
 
   url "https://github.com/NoahCzelusta/artisan/releases/download/v#{version}/Artisan-v#{version}-macos-arm64.zip"
   name "Artisan"
